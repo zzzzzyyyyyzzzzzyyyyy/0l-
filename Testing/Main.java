efc83a9f38e7d8894ce14d07254ac748c64c25a0
? <ochervonyak13@ladueschools.net>
2022-10-13 13:14:27 +0000

    ?

---
+++ Testing/Main.java
@@ -0,0 +1,10 @@
+// to turn back on : java.inlayHints.parameterNames.enabled
+// https://stackoverflow.com/questions/71865654/why-is-vs-code-showing-this-these-labels-or-parameter-names-for-system-out-print#:~:text=in%20this%20question's%20case%2C%20the,for%20print%20and%20println%20functions.
+
+package Testing;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("?");
+    }
+}
